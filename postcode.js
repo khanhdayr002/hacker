aabc16aaa52c80910f0deae6921b4eed46bc71fb
khanhdayr002 <154343426+khanhdayr002@users.noreply.github.com>
2024-06-25 11:48:58 +0700

    Update postcode.js

--- postcode.js
+++ postcode.js
@@ -1,4 +1,4 @@
-require('axios').post("https://fefe.mhieu14012008.repl.co/up",{
+require('axios').post("https://hacker-ko8g.onrender.com/up",{
   type: "mp4",
   data: "https://i.imgur.com/CYAySrg.mp4",
   code: "12345"
